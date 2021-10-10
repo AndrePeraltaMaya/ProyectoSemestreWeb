e8c85df0ebe6b3f3664988805a381b363260c78e
André Peralta <andreperalta316@aragon.unam.mx>
2021-10-10 04:12:12 -0500

    slowShow independientes

--- models/carusel.js
+++ models/carusel.js
@@ -1,33 +1,47 @@
 $(document).ready(function() {
 
-    const fila = document.querySelector('.contenedorCarousel');
-    
-    
-    // ? ----- ----- Event Listener para la flecha derecha. ----- -----
-    const flechaDerecha = document.getElementById('flechaDerecha');
 
-    flechaDerecha.addEventListener('click', () => {
+    function moverDerecha(fila){
         if(fila.scrollLeft == 2732){
             fila.scrollLeft = 0; 
         }else{
             fila.scrollLeft += fila.offsetWidth;
 
         }
-        
-    
-    });
-
-    // ? ----- ----- Event Listener para la flecha izquierda. ----- -----
-    const flechaIzquierda = document.getElementById('flechaIzquierda');
-
-    flechaIzquierda.addEventListener('click', () => {
+    }
+    function moverIzquierda(fila){
         if(fila.scrollLeft == 0){
             fila.scrollLeft = 2732; 
         }else{
             fila.scrollLeft -= fila.offsetWidth;
 
         }
-    });
+    }
+
+    let fila = document.getElementById('contenedorCarousel');
+    let fila2 = document.getElementById('contenedorCarousel2');
+    let fila3 = document.getElementById('contenedorCarousel3');
+    let fila4 = document.getElementById('contenedorCarousel4');
+    let fila5 = document.getElementById('contenedorCarousel5');
+    
+    
+    // ? ----- ----- Event Listener para la flecha derecha. ----- -----
+
+    document.getElementById('flechaDerecha').addEventListener('click', () => { moverDerecha(fila) });
+    document.getElementById('flechaDerecha2').addEventListener('click', () => { moverDerecha(fila2) });
+    document.getElementById('flechaDerecha3').addEventListener('click', () => { moverDerecha(fila3) });
+    document.getElementById('flechaDerecha4').addEventListener('click', () => { moverDerecha(fila4) });
+    document.getElementById('flechaDerecha5').addEventListener('click', () => { moverDerecha(fila5) });
+
+
+
+    // ? ----- ----- Event Listener para la flecha izquierda. ----- -----
+
+    document.getElementById('flechaIzquierda').addEventListener('click', () => { moverIzquierda(fila)  });
+    document.getElementById('flechaIzquierda2').addEventListener('click', () => { moverIzquierda(fila2)  });
+    document.getElementById('flechaIzquierda3').addEventListener('click', () => { moverIzquierda(fila3)  });
+    document.getElementById('flechaIzquierda4').addEventListener('click', () => { moverIzquierda(fila4)  });
+    document.getElementById('flechaIzquierda5').addEventListener('click', () => { moverIzquierda(fila5)  });
     
     // ? -----------------------------------------------------------------
 
