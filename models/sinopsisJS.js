b03a9fc2e977fbff739762eecbc065e61f428449
André Peralta <andreperalta316@aragon.unam.mx>
2021-10-16 03:58:10 -0500

    hoy

---
+++ models/sinopsisJS.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    let mid = document.getElementById("botonMidsommar");
+    mid.on('click', function() {
+        alert("asd");
+    });
+
+
+
+});
+
+/*
+botonWitch
+botonMandy
+botonSwallow
+
+*/
